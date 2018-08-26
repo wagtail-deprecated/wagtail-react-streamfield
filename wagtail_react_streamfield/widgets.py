67ba3c198fd67cb2ed8103696ec3521b4276a4f0
Bertrand Bordage <bordage.bertrand@gmail.com>
2018-08-26 21:48:23 +0200

    Loads initial data in a hidden textarea to prevent exit confirm dialogs.

--- wagtail_react_streamfield/widgets.py
+++ wagtail_react_streamfield/widgets.py
@@ -1,16 +1,45 @@
 import json
+from json import JSONEncoder
 from uuid import uuid4
 
 from django.forms import Media
 from django.utils.safestring import mark_safe
 from wagtail.core.blocks import (
     BlockWidget, StructBlock, ListBlock, FieldBlock, RichTextBlock,
-    StreamBlock,
+    StreamBlock, ChooserBlock,
 )
 
 
-def to_json_script(data):
-    return json.dumps(data).replace('<', '\\u003c')
+class ConfigJSONEncoder(JSONEncoder):
+    def default(self, o):
+        if isinstance(o, BlockData):
+            return o.data
+
+
+class InputJSONEncoder(JSONEncoder):
+    def default(self, o):
+        if isinstance(o, BlockData):
+            return {'id': o['id'],
+                    'type': o['type'],
+                    'value': o['value']}
+        return super().default(o)
+
+
+def to_json_script(data, encoder=ConfigJSONEncoder):
+    return json.dumps(
+        data, separators=(',', ':'), cls=encoder
+    ).replace('<', '\\u003c')
+
+
+class BlockData:
+    def __init__(self, data):
+        self.data = data
+
+    def __getitem__(self, item):
+        return self.data[item]
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
 
 
 class NewBlockWidget(BlockWidget):
@@ -21,39 +50,41 @@ class NewBlockWidget(BlockWidget):
         if isinstance(block, StructBlock):
             children_errors = ({} if errors is None
                                else errors.as_data()[0].params)
-            value = [self.prepare_value(block, block.child_blocks[k], v,
-                                        type_name=k,
-                                        errors=children_errors.get(k))
-                     for k, v in value.items()
-                     if k in block.child_blocks]
+            value = [
+                self.prepare_value(
+                    block, block.child_blocks[k], v, type_name=k,
+                    errors=children_errors.get(k))
+                for k, v in value.items()
+                if k in block.child_blocks]
         elif isinstance(block, ListBlock):
             children_errors = ([None] * len(value) if errors is None
                                else errors.as_data()[0].params)
-            value = [self.prepare_value(block, block.child_block,
-                                        child_block_data, type_name=block.name,
-                                        errors=child_errors)
-                     for child_block_data, child_errors
-                     in zip(value, children_errors)]
+            value = [
+                self.prepare_value(
+                    block, block.child_block, child_block_data,
+                    type_name=block.name, errors=child_errors)
+                for child_block_data, child_errors
+                in zip(value, children_errors)]
         elif isinstance(block, StreamBlock):
             children_errors = ({} if errors is None
                                else errors.as_data()[0].params)
-            value = [self.prepare_value(block, child_block_data.block,
-                                        child_block_data.value,
-                                        errors=children_errors.get(i))
-                     for i, child_block_data in enumerate(value)]
+            value = [
+                self.prepare_value(
+                    block, child_block_data.block, child_block_data.value,
+                    errors=children_errors.get(i))
+                for i, child_block_data in enumerate(value)]
         if parent_block is None:
             return value
-        data = {
+        data = BlockData({
+            'id': str(uuid4()),
             'type': type_name,
             'hasError': bool(errors),
-        }
+        })
+        if isinstance(block, ChooserBlock):
+            data['html'] = block.render_form(value, prefix='field-__ID__',
+                                             errors=errors)
         if isinstance(block, FieldBlock):
             from wagtail.admin.rich_text import DraftailRichTextArea
-            data.update(
-                id=str(uuid4()),
-                html=block.render_form(value, prefix='field-__ID__',
-                                       errors=errors),
-            )
             if value == '':
                 value = None
             value = block.value_for_form(block.field.prepare_value(value))
@@ -72,9 +103,13 @@ class NewBlockWidget(BlockWidget):
             'value': self.prepare_value(None,
                                         self.block_def, value, errors=errors),
         }
+        escaped_value = to_json_script(streamfield_config['value'],
+                                       encoder=InputJSONEncoder)
         return mark_safe("""
         <script type="application/json" data-streamfield="%s">%s</script>
-        """ % (name, to_json_script(streamfield_config)))
+        <textarea style="display: none;" name="%s">%s</textarea>
+        """ % (name, to_json_script(streamfield_config),
+               name, escaped_value))
 
     @property
     def media(self):
